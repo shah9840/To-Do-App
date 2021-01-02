bec5f294823af22dd34f0c570da2666038c25e11
syed shahabaaz ahmed <shahabaazahmedsyed@gmail.com>
2021-01-02 17:18:08 +0530

    First commit

---
+++ public/browser.js
@@ -0,0 +1,67 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+
+
+// home page loader
+
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item)
+}).join('')
+
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
+
+// create feature
+
+let createField = document.getElementById("create-field")
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    axios.post('/create-item', { text: createField.value }).then(function(response) {
+
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch()
+
+})
+
+
+
+document.addEventListener("click", function(e) {
+    // delete
+
+    if (e.target.classList.contains("delete-me")) {
+       if (confirm("Are You Sure")) {
+            axios.post('/delete-item', { id: e.target.getAttribute("data-id") }).then(function() {
+                e.target.parentElement.parentElement.remove()
+            }).catch(function() {
+                res.send("Failed to delete!")
+            })
+       }
+    }
+
+
+
+    // update/ modify
+    if (e.target.classList.contains("edit-me")) {
+        let userInput = prompt("Enter New value", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+        
+        if (userInput) { 
+            axios.post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") }).then(function() {
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+            }).catch(function() {
+                console.log("Try again later!!")
+            })
+        }
+    }
+})
+
